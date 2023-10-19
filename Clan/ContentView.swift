3112a6be06e2f53ada66009621cf7490a00efdb6
Mubeen Riaz <mobeenriaz89@gmail.com>
2023-10-19 20:01:08 +0500

    -removed pods and added firebase using swift package manager. -created dynamic custom views for buttons and text fields

--- Clan/ContentView.swift
+++ Clan/ContentView.swift
@@ -20,12 +20,12 @@ struct ContentView: View {
                     Spacer()
                     
                     NavigationLink(destination: LoginView()){
-                        ButtonView(text:"Login")
+                        CustomButton(text:"Login")
                     }.background(.black)
                         .cornerRadius(10)
                         .padding(8)
                     NavigationLink(destination: SignupView()){
-                        ButtonView(text:"Signup")
+                        CustomButton(text:"Signup")
                     }
                     .background(.black)
                     .cornerRadius(10)
@@ -40,18 +40,4 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-}
-
-struct ButtonView: View {
-    let text: String
-    var body: some View {
-            Text(text)
-                .background(.black)
-                .tint(.white)
-                .font(.custom("futura", size: 20))
-                .padding(20)
-                .frame(width: 200,height: 50)
-        
-        
-    }
 }
---
+++ Clan/CustomViews/CustomButton.swift
@@ -0,0 +1,25 @@
+//
+//  CustomButton.swift
+//  Clan
+//
+//  Created by Mubeen Riaz on 03/10/2023.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .background(.black)
+            .tint(.white)
+            .font(.custom("futura", size: 20))
+            .padding(20)
+            .frame(width: 200,height: 50)
+    }
+}
+
+#Preview {
+    CustomButton(text: "Button text")
+}
